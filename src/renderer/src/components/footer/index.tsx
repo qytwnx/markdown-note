75ada68c96e742343c3cb0dd1747da5cba5ee3ba
QYT-WNX <1729566829@qq.com>
2024-06-09 23:47:20 +0800

    feat: Add version

--- src/renderer/src/components/footer/index.tsx
+++ src/renderer/src/components/footer/index.tsx
@@ -1,10 +1,11 @@
 import styles from './index.module.less';
+import Config from '../../../../../package.json';
 
 const Footer = () => {
   return (
     <>
       <footer className={styles['footer-container']}>
-        Copyright © 2024 - All right reserved by QYT-WNX
+        v{Config.version} Copyright © 2024 - All right reserved by QYT-WNX
       </footer>
     </>
   );
